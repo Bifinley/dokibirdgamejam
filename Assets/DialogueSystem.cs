891e866d08129dd92b5699c4e6a5c30a8efd673c
unknown <mrbifinley@gmail.com>
2025-08-11 18:51:01 -0500

    Changed Dragoon Texture in cutscene1 + reordering them

--- Assets/DialogueSystem.cs
+++ Assets/DialogueSystem.cs
@@ -7,14 +7,30 @@ public class DialogueSystem : MonoBehaviour
     [SerializeField] private string[] dialogueMessage;
 
     [SerializeField] private int currentDialogueIndex;
+    [SerializeField] private int currentDialogueIndexMax;
 
     [SerializeField] private TMP_Text dialogueText;
 
+    private void Start()
+    {
+        currentDialogueIndex = -1;
+        currentDialogueIndexMax = dialogueMessage.Length -1;
+
+        Debug.Log(dialogueMessage.Length);
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && currentDialogueIndex != currentDialogueIndexMax)
         {
-            currentDialogueIndex++;
+            if (currentDialogueIndex > currentDialogueIndexMax)
+            {
+                currentDialogueIndex = currentDialogueIndexMax;
+            }
+            else if(currentDialogueIndex <= currentDialogueIndexMax) 
+            {
+                currentDialogueIndex++;
+            }
             dialogueText.text = $"{dialogueMessage[currentDialogueIndex]}"; // goes through all the messages by the NPC in the array
         }
     }
