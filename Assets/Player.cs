88a68eeaf6db861a39a1a3f66dcfe68e53ea60f7
unknown <mrbifinley@gmail.com>
2025-08-11 00:57:25 -0500

    basic movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private Transform PlayerTransform;
 
     [SerializeField] private Vector2 playerInput = new Vector2(0, 0);
+    [SerializeField] private Vector2 moveOnY = new Vector2(0, 0);
 
     private void Start()
     {
@@ -12,16 +14,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            playerInput.y = 4;
+            playerInput.y = 1;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            playerInput.y = -4;
+            playerInput.y = -1;
+        }else if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            playerInput.y = 0;
         }
 
-        PlayerTransform.position = playerInput;*/
+        moveOnY = new Vector2(0, playerInput.y) * 5 * Time.deltaTime;
+
+        PlayerTransform.position += new Vector3(0,moveOnY.y,0);
     }
 }
 
