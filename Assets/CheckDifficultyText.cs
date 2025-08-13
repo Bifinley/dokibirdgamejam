0a816eda94f224e1e82224e3947f16c534b75fdd
Bleu Finley <mrbifinley@gmail.com>
2025-08-13 18:09:17 -0500

    Updated CutScene Level 1 to just CutScene, changing character based off Difficulty (Still in the works)

--- Assets/CheckDifficultyText.cs
+++ Assets/CheckDifficultyText.cs
@@ -3,9 +3,26 @@ using UnityEngine;
 
 public class CheckDifficultyText : MonoBehaviour
 {
+    private enum Dragoons
+    {
+        EggGoon,
+        Dragoon,
+        LongGoon,
+        ChonkyGoon
+    }
+
     MainMenu.GameDifficulty gameDifficulty;
     [SerializeField] TMP_Text setDifficultyText;
 
+    [SerializeField] GameObject[] dokiDragoons;
+
+    private void Start()
+    {
+        dokiDragoons[(int)Dragoons.EggGoon].SetActive(false);
+        dokiDragoons[(int)Dragoons.Dragoon].SetActive(false);
+        dokiDragoons[(int)Dragoons.LongGoon].SetActive(false);
+        dokiDragoons[(int)Dragoons.ChonkyGoon].SetActive(false);
+    }
 
     private void Awake()
     {
@@ -14,5 +31,17 @@ public class CheckDifficultyText : MonoBehaviour
     private void Update()
     {
         setDifficultyText.text = $"Difficulty: {gameDifficulty}";
+
+        switch(gameDifficulty)
+        {
+            case MainMenu.GameDifficulty.Easy:
+                dokiDragoons[(int)Dragoons.EggGoon].SetActive(true); break;
+            case MainMenu.GameDifficulty.Normal:
+                dokiDragoons[(int)Dragoons.Dragoon].SetActive(true); break;
+            case MainMenu.GameDifficulty.Medium:
+                dokiDragoons[(int)Dragoons.LongGoon].SetActive(true); break;
+            case MainMenu.GameDifficulty.Hard:
+                dokiDragoons[(int)Dragoons.ChonkyGoon].SetActive(true); break;
+        }
     }
 }
